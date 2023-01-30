c7908e5de71d20082ea49d6b06c4e7197643cba6
joanne gangapal <joannegangapal@gmail.com>
2023-01-30 16:43:15 +0100

    Continuité du 2048 flèche directionnel bas

--- Jeux/2048.py
+++ Jeux/2048.py
@@ -1,8 +1,21 @@
-
+from pynput import keyboard
 import numpy as np
 import random as rd
-from pynput.keyboard import Key, Controller
 
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
 
 def inittab():
     grille=[]
@@ -12,6 +25,9 @@ def inittab():
     for i in range (0,2):
         premierchiffre=rd.randint(0,3)
         secondchiffre=rd.randint(0,3)
+        while grille[premierchiffre][secondchiffre]!='*':
+            premierchiffre=rd.randint(0,3)
+            secondchiffre=rd.randint(0,3)
         grille[premierchiffre][secondchiffre]=rd.choice(liste_chiffrededepart)
     return grille
 
@@ -22,17 +38,33 @@ def affichertab(grille):
             print(grille[i][j],end='\t')
         print('\n')
 
-def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+def deplacement(grille):
+    mouv=keyboard.Key.esc
+    with keyboard.Events() as events:
+        for event in events:
+            if event.key == keyboard.Key.up or event.key == keyboard.Key.down or event.key == keyboard.Key.left or event.key == keyboard.Key.right:
+                mouv=event.key
+                break
+    if mouv==keyboard.Key.down:
+        for i in range(len(grille)):
+            for j in range(len(grille[i])):
+                flag=True
+                choix=grille[len(grille)-1-i][j]
+                k=i
+                while flag:
+                    flag=False
+                    if grille[len(grille)-1-k][j]!='*' and len(grille)-1-k+1<len(grille) and grille[len(grille)-1-k+1][j]=='*':
+                        grille[len(grille)-1-k+1][j]=grille[len(grille)-1-k][j]
+                        grille[len(grille)-1-k][j]='*'
+                        k+=1
+                        flag=True
 
-def coup(grille):
-    on_press()
+    print('\n\n')
+    return grille
+
+
 
 grille=inittab()
 affichertab(grille)
-coup(grille)
+deplacement(grille)
+affichertab(grille)
